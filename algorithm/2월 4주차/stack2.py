57c16d4e9855e7c4fcf1623c0dc1eca648b1b197
junghyeon <jjdye02361@gmail.com>
2021-02-24 12:29:26 +0900

    0224 TIL

---
+++ algorithm/2월 4주차/stack2.py
@@ -0,0 +1,17 @@
+s = []
+
+s.append(1)
+s.append(2)
+s.append(3)   # push
+
+#pop하기전에는 항상 empty 체크
+if s:
+    print(s.pop())
+if s:
+    print(s.pop())
+if s:
+    print(s.pop())
+if s:
+    print(s.pop())
+
+
